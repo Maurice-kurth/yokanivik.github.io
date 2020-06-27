ed3b337c3418b0f2cec82bc552b067b35a9f4237
Maurice K. | Yoka <48809800+YokaNivik@users.noreply.github.com>
2020-06-27 18:48:23 +0200

    Add files via upload
    Portfolio 27/06/20

--- README.txt
+++ README.txt
@@ -1,1 +1,1 @@
-Maurice Kurth Portfolio website created through a html5UP template and with additional CSS for customization
+Maurice Kurth Portfolio
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,29 +4,63 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
-		});
+	breakpoints({
+		xlarge: ['1141px', '1680px'],
+		large: ['981px', '1140px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['321px', '480px'],
+		xxsmall: [null, '320px']
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Scrolly.
-		$('.scrolly').scrolly();
+	$('.scrolly').scrolly();
 
 })(jQuery);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = slides.length
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
